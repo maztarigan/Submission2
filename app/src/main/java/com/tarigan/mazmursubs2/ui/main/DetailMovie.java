94bf7eab0d43e41622e6413d8455993f9522b2b1
Masmur Tarigan <dev.mazmur@gmail.com>
2019-09-11 09:41:54 +0700

    Perbaikan DetailMovie.java

--- app/src/main/java/com/tarigan/mazmursubs2/ui/main/DetailMovie.java
+++ app/src/main/java/com/tarigan/mazmursubs2/ui/main/DetailMovie.java
@@ -13,12 +13,15 @@ import com.tarigan.mazmursubs2.R;
 public class DetailMovie extends AppCompatActivity {
     public static final String EXTRA_MOVIE = "extra_movie";
     private TextView tvName, tvDescription;
-    private ImageView tvPhoto;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_movie);
+
+//        Rekomendasi reviewer ubah menjadi variabel lokal
+        ImageView tvPhoto;
 
         tvName = findViewById(R.id.txt_name);
         tvDescription = findViewById(R.id.txt_descripton);
@@ -26,14 +29,16 @@ public class DetailMovie extends AppCompatActivity {
 
         Movie movie = getIntent().getParcelableExtra(EXTRA_MOVIE);
         String name = movie.getName();
-        String description = movie.getDesc();
-        String photo = movie.getPhoto();
+//        Rekomendasi reviewer untuk menghindari kode yg berpotensi NullPointerException
+        if (movie != null) {
+            String description = movie.getDesc();
+            String photo = movie.getPhoto();
 
-        tvName.setText(name);
-        tvDescription.setText(description);
-        Glide.with(this)
-                .load(photo)
-                .into(tvPhoto);
-
+            tvName.setText(name);
+            tvDescription.setText(description);
+            Glide.with(this)
+                    .load(photo)
+                    .into(tvPhoto);
+        }
     }
 }
