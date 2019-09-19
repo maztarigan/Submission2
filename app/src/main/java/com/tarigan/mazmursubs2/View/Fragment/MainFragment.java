323d0c7bd1f9407d34d097d054c445e97c626cfd
Masmur Tarigan <dev.mazmur@gmail.com>
2019-09-19 21:45:06 +0700

    Favorite

--- app/src/main/java/com/tarigan/mazmursubs2/View/Fragment/MainFragment.java
+++ app/src/main/java/com/tarigan/mazmursubs2/View/Fragment/MainFragment.java
@@ -1,20 +1,26 @@
 package com.tarigan.mazmursubs2.View.Fragment;
 
+import android.content.ClipData;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.view.menu.MenuView;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabLayout;
 import com.tarigan.mazmursubs2.R;
 import com.tarigan.mazmursubs2.Adapter.SubmissionAdapter;
+import com.tarigan.mazmursubs2.View.Activity.DetailMovie;
 
 public class MainFragment extends Fragment {
     private SubmissionAdapter submissionAdapter;
@@ -22,11 +28,11 @@ public class MainFragment extends Fragment {
     private static Context context;
 
 
+
     public static Fragment newInstance(Context contexts) {
         context = contexts;
         return new MainFragment();
     }
-
 
     @Nullable
     @Override
@@ -44,6 +50,24 @@ public class MainFragment extends Fragment {
         TabLayout tabLayout = view.findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(viewPager);
 
+        BottomNavigationView navigation = (BottomNavigationView) view.findViewById(R.id.navigation_favorite);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()){
+                case R.id.navigation_favorite:
+//                    menuItem.setIcon(getResources().getDrawable(R.drawable.ic_favorite_red_24dp));
+                    Intent intent = new Intent(context, DetailMovie.class);
+                    startActivity(intent);
+                    return true;
+            }
+            return false;
+        }
+    };
 
 }
